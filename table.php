14d594530ac6e5a98a20814685068941749ab79a
Jonas Pettersson <jonas.pettersson88@gmail.com>
2023-10-01 15:04:51 +0200

    adjusted table loop and if

--- table.php
+++ table.php
@@ -7,6 +7,40 @@ require __DIR__ . '/data.php';
 <table>
     <thead>
         <tr>
+            <th>Teams</th>
+            <th>League</th>
+            <th>Last time champions</th>
+            <th>City</th>
+            <th>Nickname</th>
+            <th>Website</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach ($teams as $key => $value) : ?>
+            <tr>
+                <td><?php echo $key ?></td>
+                <td><?php echo $value['league'] ?></td>
+                <td><?php echo $value['last-time-champions'] ?>
+                    <?php if ($value['last-time-champions'] == null) {
+                        echo 'N/A';
+                    } ?></td>
+                <td><?php echo $value['city'] ?></td>
+                <td><?php if (empty($value['nickname'])) {
+                        echo 'N/A';
+                    } else {
+                        echo $value['nickname'];
+                    } ?></td>
+                <td><?php echo $value['url'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<?php /*
+<table>
+    <thead>
+        <tr>
             <td>Teams</td>
             <?php foreach ($teams as $key => $value) : ?>
                 <th><?php echo $key ?></th>
@@ -55,3 +89,4 @@ require __DIR__ . '/data.php';
         </tr>
     </tbody>
 </table>
+*/ ?>
