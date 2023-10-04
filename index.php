0c8af59acf19a5f2aa9a0d64494875b046783286
Jonas Pettersson <jonas.pettersson88@gmail.com>
2023-10-04 14:23:35 +0200

    added echos

--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 require __DIR__ . '/head.php';
 require __DIR__ . '/table.php';
 require __DIR__ . '/city-values.php';
+require __DIR__ . '/number-of-teams.php';
+
+?>
+<br />
+<?php
+echo $totalTeams;
+?>
+<br />
+<?php
+echo $cityValue;
