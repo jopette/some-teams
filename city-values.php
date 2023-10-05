100ce84efcd79372af9e662066f9b8862034f1b2
Jonas Pettersson <jonas.pettersson88@gmail.com>
2023-10-05 09:56:42 +0200

    added comments

--- city-values.php
+++ city-values.php
@@ -6,16 +6,17 @@ require __DIR__ . '/data.php';
 
 <?php
 
-$cityValues = [];
-
+//foreach loop fetches the city values and fills an empty array
 foreach ($teams as $key => $value) :
 
     $cityValues[] = $value['city'];
 
 endforeach;
 
+//array_unique removes duplicate values and fills a new empty array
 $uniqueValues[] = array_unique($cityValues);
 
+//new values are iterated and implode returns a string from the array elements
 foreach ($uniqueValues as $uniqueValue) :
 
     $cityValue = implode(' ', $uniqueValue);
